30795e9382f22482d083cf14b4c66cb2335213d3
wheat19940527 <313808267@qq.com>
2019-11-27 22:40:16 +0800

    add index and bootstrap

--- src/main/java/com/mqm/community/controller/HelloController.java
+++ src/main/java/com/mqm/community/controller/HelloController.java
@@ -8,10 +8,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-     public String hello(@RequestParam(name = "name") String name, Model model)
+//    @GetMapping("/")
+//     public String hello(@RequestParam(name = "name") String name, Model model)
+//    {
+//        model.addAttribute("name",name);
+//        return "index";
+//    }
+
+    @GetMapping("/")
+    public String index()
     {
-        model.addAttribute("name",name);
-        return "hello";
+
+        return "index";
     }
 }
